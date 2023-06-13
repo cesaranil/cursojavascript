4a3f02434b90ca8977a8f8649305b4dd6cff173c
Cesaranil8 <8182cesar17@gmail.com>
2023-06-13 12:41:27 -0500

    Empezando Curso de JavaScript

---
+++ introduccion/variables.js
@@ -0,0 +1,14 @@
+var edad = 27;
+
+
+let nombre = "Cesar Orellana";
+const correo = "cesar@cesar.com";
+
+let telefono, pais, id;
+telefono = "65373827";
+//console.log(telefono);
+pais = "Venezuela";
+id = "500";
+
+const resultado = 4 + 4;
+console.log(resultado)
