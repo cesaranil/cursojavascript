c4e55a9e9e23be4dc95e9c8c99ec01b816f9fb1f
Cesaranil8 <8182cesar17@gmail.com>
2023-06-14 20:12:30 -0500

    Los templateString

---
+++ introduccion/templateString.js
@@ -0,0 +1,7 @@
+const nombre = 'Cesar Orellana';
+const edad = 27;
+const pais = 'Venezuela';
+
+console.log('La persona es ' + nombre + ' tiene ' + edad + ' y es de ' + pais);
+
+console.log(`La persona es ${nombre} tiene ${edad} y es de ${pais}`);
