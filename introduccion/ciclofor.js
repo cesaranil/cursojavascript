4abf64d7f69fdf1c8d5ac8be1307ebb3c7f5c386
Cesaranil8 <8182cesar17@gmail.com>
2023-10-04 19:45:53 -0500

    Ciclo For

---
+++ introduccion/ciclofor.js
@@ -0,0 +1,9 @@
+const nombres = ['Carlos', 'Rafael', 'Estefania', 'Rodrigo', 'Rafael', 'Gema', 'Diana', 'Sara']
+//nombres.forEach((nombre) => {
+//    console.log(nombre)
+//})
+
+for( numero = 1; numero <=100; numero++){
+    console.log(numero)
+
+}
