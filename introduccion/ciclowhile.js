aeebc6bc6566646aa6f6b5a3bb8499886db9f39a
Cesaranil8 <8182cesar17@gmail.com>
2023-10-10 20:53:51 -0500

    Ciclo While

---
+++ introduccion/ciclowhile.js
@@ -0,0 +1,22 @@
+/*
+	📌 Ciclo While
+	Es similar a for, pero con la diferencia de que solo tenemos un condicional.
+	Mientras se cumpla el condicional se ejecutara el ciclo.
+*/
+
+/**let i = 0
+while(i <= 10){
+    console.log(i)
+    i++
+}*/
+
+/*
+	📌 Ciclo Do While
+	Es similar al ciclo while, con la diferencia de que ejecuta el bloque de código al menos una vez.
+*/
+
+let i = 0;
+do{
+    console.log(i);
+    i++;
+}while(i <= 10)
